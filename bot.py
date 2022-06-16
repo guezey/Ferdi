759e00ae9a075ce09d78673b4748d29ae8d60528
Ali Güzey <aliemirguzey@gmail.com>
2022-06-16 15:12:55 +0300

    Add cosmetics to roller

--- bot.py
+++ bot.py
@@ -16,9 +16,12 @@ async def on_ready():
 
 
 @bot.command(name='roll', help='Rolls some dice', usage="\n\nUse format: 'd6+2d4+10'"
-                                                        "\nYou can use negative constant modifiers as: 'd20+-12'"
-                                                        " Put that damn plus everywhere. Please."
-                                                        "\nDo *NOT* put spaces in your expression")
+                                                        "\nYou can use negative modifiers as: 'd20+-12'"
+                                                        "\nPut that damn plus between everything. Please."
+                                                        "\nNo parentheses. Why would you use them anyway?"
+                                                        "\n\nDo *NOT* put spaces in your expression."
+                                                        "\nEverything *MUST* be next to each other."
+                                                        "\nOtherwise rolls only the first one")
 async def roll_dice(ctx, dice: str):
     try:
         result = list()
@@ -28,8 +31,14 @@ async def roll_dice(ctx, dice: str):
             if die.find('d') > 0:
                 sub_result = list()
                 for _ in range(int(die.split('d')[0])):
-                    sub_result.append(str(random.randint(1, int(die.split('d')[1]))))
-                    sum += int(sub_result[-1])
+                    res = random.randint(1, int(die.split('d')[1]))
+                    sum += res
+                    if res == int(die.split('d')[1]):
+                        sub_result.append(f"***{res}***")
+                    elif res == 1:
+                        sub_result.append(f"__{res}__")
+                    else:
+                        sub_result.append(str(res))
                 result.append(f"({', '.join(sub_result)})")
             elif die.find('d') == 0:
                 res = random.randint(1, int(die[1:]))
@@ -40,20 +49,12 @@ async def roll_dice(ctx, dice: str):
                 sum += int(die)
         result.append(f'Total: {sum}')
         await ctx.send(' | '.join(result))
-        # command = (dice.split('d')[0], dice.split('d')[1])
-        # results = list()
-        # sum = 0
-        # for _ in range(int(command[0])):
-        #     results.append(str(random.randint(1, int(command[1]))))
-        #     sum += int(results[-1])
-        # results.append(f'Total: {sum}')
-        # await ctx.send(', '.join(results))
-    except:
-        await ctx.send("Haha, no. Try again in a different format maybe?")
+    except ValueError:
+        await ctx.send("Haha, no. Try again in a different format maybe? *or check `>help roll`*")
         raise
 
 
-@bot.command(name='twss', help='- That\'s what she said!')
+@bot.command(name='twss', help='That\'s what she said!')
 async def twss(ctx):
     await ctx.send(file=discord.File(open('the-office-thats-what-she-said.gif', 'rb')))
 
